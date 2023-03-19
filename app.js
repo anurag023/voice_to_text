6a2b6b9a116406e08ba6091254637d04d518dab9
Anurag <theunstoppableanurag@gmail.com>
2023-03-19 18:39:11 +0530

    some few channges

--- app.js
+++ app.js
@@ -9,13 +9,13 @@ app.use('/static', express.static('static')) // For serving static files
 app.use(express.urlencoded())
 
 //PUG SPECIFIC STUFF
-app.set('view engine', 'html') // Set the template engine as pug
+app.set('view engine', 'pug') // Set the template engine as pug
 app.set('views', path.join(__dirname, 'views')) // Set the views directory
 
 
 app.get(`/`,  (req,res)=>{ 
     const params= { }
-     res.status(200).render(`index.html`, params);
+     res.status(200).render(`index.pug`, params);
 })
 
 app.listen(port, ()=>{
